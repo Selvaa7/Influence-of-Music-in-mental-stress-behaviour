17cf75c5ff20edfec9134511a2f3336f5fddb1fc
Selva <selventhiran76@gmail.com>
2021-07-13 15:45:09 +0800

    Update project_config.py

--- project_config.py
+++ project_config.py
@@ -1,24 +1,24 @@
 import os
-str = "os.name"
-resamp_val=200
-bandpass_fmin=1
-bandpass_fmax=50
-set_data = dict ( channels='channels.tsv',
-                  eeg_edf='eeg.edf',
-                  eeg_json='eeg.json',
-                  events_json='events.json',
-                  event_tsv='events.tsv' )
+
+resamp_val = 200
+bandpass_fmin = 1
+bandpass_fmax = 50
+set_data = dict(channels='channels.tsv',
+                eeg_edf='eeg.edf',
+                eeg_json='eeg.json',
+                events_json='events.json',
+                event_tsv='events.tsv')
 
 # link to download all experiment file
 base_url = 'https://openneuro.org/crn/datasets/ds002721/snapshots/1.0.1/files/sub-'
 
-myhost = os.name()[1]
+myhost = os.name[1]
 
 if myhost == 'rpb':
     base_folder = '/mnt/d/data_set/music_eeg/'
 elif myhost == 'cisir2':
     base_folder = '/home/cisir2/Documents/rpb/music_eeg/'
-elif myhost=='sel':
-    base_folder = 'C:\Users\User\Documents\EEG Data'
+elif myhost == 'nt':
+    base_folder = r'C:\Users\User\Documents\EEG Data'
 else:
-    raise Value ('C:\Users\User\Documents\EEG Data\New')
+    raise TypeError(r'C:\Users\User\Documents\EEG Data')
